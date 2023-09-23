9ee5572ffba134eee7a9ef40822055ab28bba335
bm9avan <mggpavan@gmail.com>
2023-09-23 21:57:38 +0530

    fixing issue

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 const jobs = require("./Jobs");
 
 const app = express();
 app.use(express.json());
-app.use(express.static("html"));
+app.use(express.static(path.join(__dirname + "/html")));
 
 mongoose.connect(process.env.MONGO_URI);
 
