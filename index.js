7d5b6bf30f46c916b2b9f6fdbf05ee26640eaa6d
PAVAN BASAPATHI <mggpavan@gmail.com>
2023-09-29 10:01:29 +0530

    Allow-Headers
    https://stackoverflow.com/questions/32500073/request-header-field-access-control-allow-headers-is-not-allowed-by-itself-in-pr

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ mongoose.connect(process.env.MONGO_URI);
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
